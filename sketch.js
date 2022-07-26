0a5952826a82feb4471392adc63ee6d5a2a54fc6
AchyutKakula <109831618+AchyutKakula@users.noreply.github.com>
2022-07-26 16:30:02 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var weight = [90,92,94,98,96]
+var total  = weight[0] + weight[1] + weight[2] + weight[3] + weight[4]
+var average  = total/weight.length;
+console.log(average);
+
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
